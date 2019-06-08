ca187e0458e7dfe1d9296b6ad5494186798e9139
vannghia17 <vannghiapt97@gmail.com>
2019-06-08 14:09:49 +0700

    asdasd

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-require('./db');
 
 var app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/user');
 
 /* GET users listing. */
 router.get('/', async function(req, res, next) {
-  console.log('TCL: req', req);
-  try {
-    const users = await User.find();
-    console.log(users);
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(err.code || 500).json({ message: err.message });
-  }
-});
-
-router.post('/', async function(req, res, next) {
-  try {
-    console.log('TCL: req.body', req.body);
-    const { name } = req.body;
-    await User.create(
-      {
-        name
-      },
-      (err, user) => {
-        if (err) throw err;
-      }
-    );
-    res.status(200).json({ name });
-    console.log(user);
-  } catch (err) {
-    res.status(err.code || 500).json({ message: err.message });
-  }
+  res.render('users', { title: 'Users'});
 });
 
 module.exports = router;
